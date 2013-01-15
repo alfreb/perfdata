4da26e1df89d77cabe35b4fc4d885f3a8e66bf54
Alfred Bratterud <alfred.bratterud@hioa.no>
2013-01-15 22:23:28 +0100

    Collecting cpu-data OK

--- perfdata.cpp
+++ perfdata.cpp
@@ -1,59 +1,119 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
+#include <vector>
 #include <string>
 
 
 
 using namespace std;
 
-class cpuSample{
-  int time;
-  int cpu_user;
-  int cpu_nice;
-  int cpu_system;
-  int cpu_idle;
-  int cpu_iowait;
-  int cpu_irq;
-  int cpu_softirq;
-  int cpun_max;
-  int cpun_min;  
-  friend class perfCollector;
+//Number of columns pr. cpu in /proc/stat
+#define CPU_PROP_COUNT 7
+
+//Names for the columns
+enum cpu_prop {USER,NICE,SYSTEM,IDLE,IOWAIT,IRQ,SOFTIRQ}; 
+
+/*
+  Performance data from a single CPU, at one point in time
+*/
+class cpuSample{  
+  static int prop_count;
+  static string prop_names[CPU_PROP_COUNT];
+  long props[CPU_PROP_COUNT];  
+  string rest;
+  string cpu_name;
+  friend class cpuDataPoint;
+  
+public:
+  cpuSample(ifstream &fs,string name);
+  
 };
 
-class perfCollector{
+
+/*
+  A collection of all interesting CPU data, for one point in time
+  - Contains cpuSamples for each cpu, as well as other /proc/stat info
+  - Currently this class is just a structured representation of /proc/stat
+*/
+class cpuDataPoint{
+  long sample_time;
+  vector<cpuSample> cpuSamples;
+  long contextSwiches;
+  long bootTime;
+  long processCount;
+  long procs_running;
+  long procs_blocked;
+  
   ifstream procstat;
-  void getCPUData();
+public:
+  cpuDataPoint();
+};
+
+
+
+string cpuSample::prop_names[]={"User","nice","system","idle","iowait","irq","softirq"};
+int cpuSample::prop_count=CPU_PROP_COUNT;
+
+cpuSample::cpuSample(ifstream &fs,string name){  
+  cout << "Getting cpu data " << endl;
+  cpu_name=name;
+  cout << "Cpu: " << name << " ";
+  for(int i=0;i<prop_count;i++){
+    fs >> props[i];
+    cout << prop_names[i] << ": " << props[i] << " ";
+  }
+  cout << endl;
+  getline(fs,rest);
+}
+
+
+class memSample{
+  int time;
+};
+
+
+class perfCollector{  
+  //cpuDataPoints cpu;
+  //memDataPoints mem
 public:
   void getData();  
 };
   
 void perfCollector::getData(){
-  getCPUData();
+  cpuDataPoint cpuData;
 };
 
-void perfCollector::getCPUData(){
-  string line;
-  string word;
-  stringstream ss;
-  cpuSample cpu;
+cpuDataPoint::cpuDataPoint(){
+  string firstWord;
+  string rest;
+  sample_time=time(0);
   procstat.open("/proc/stat");
+  //  cout << "Started at " << sample_time << " ";
   while(procstat.good()){
-    procstat >> word; //get first word 
-    if(word=="cpu"){
-      //Could get rest of the line now, with:
-      //getline(procstat,line);      
-      procstat >> cpu.
-      cout << "Cpu : " << line << endl;
-    }    
+    procstat >> firstWord; //get first word 
+    if(firstWord.substr(0,3)=="cpu"){
+      //Create a cpu-sample, add it to the vector
+      //This requires the couSample constructor to consume a whole line from procstat
+      cpuSamples.push_back(cpuSample(procstat,firstWord));
+    }else{
+      
+      //Here the other properties should be gathered...
+      contextSwiches=bootTime=processCount=procs_running=procs_blocked=0;
+      // ...
+      //...And then the rest of the line must be consumed
+      getline(procstat,rest);
+    }
   }
+  cout << "Done" << endl;
   procstat.close();
+  
 };
 
 int main(){
+
   perfCollector perf;
   perf.getData();
-  perf.getData();
+  //perf.getData();
   cout << "hey" << endl;
 }
